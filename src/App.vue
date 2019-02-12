058fa1d3fc3c8ad7a80cbe80c61b96318a9eea76
Allen Liu <lhard@qq.com>
2019-02-12 17:08:11 +0800

    ui init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
-    <button>红色</button>
-    <button>黑色</button>
-    <button>白色</button>
+    <button @click="changeSkin('red')">红色</button>
+    <button @click="changeSkin('black')">黑色</button>
+    <button @click="changeSkin('white')">白色</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    changeSkin (type) {
+      if (type === 'red') {
+        console.log('red')
+      } else if (type === 'black') {
+        console.log('black')
+      } else if (type === 'white') {
+        console.log('white')
+      }
+    },
+    loadStyle () {
+      // const link = document.createElement('link')
+      // link.rel = 'stylesheet';
+      // link.href = '';
+    }
+  }
 }
 </script>
 

a389809afb7598a208a6a9db6f9b66eec7b48845
Allen Liu <lhard@qq.com>
2019-04-12 16:04:09 +0800

    页面样式优化

--- src/views/main/Info.vue
+++ src/views/main/Info.vue
@@ -1,10 +1,18 @@
 <template>
-  <span>点击按钮改变背景色</span>
+  <div class="info">
+    <span>点击按钮改变背景色</span>
+    <span class="word">info test</span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-span {
-  display: block;
-  color: red;
+.info /deep/ {
+  span {
+    display: block;
+    color: red;
+  }
+  .word {
+    font-size: 12px;
+  }
 }
 </style>

d2720c40aa0a2c62352c99d7e02f3d239e894d4a
Allen Liu <lhard@qq.com>
2019-04-12 17:55:39 +0800

    优化生成配置文件命令

--- build/build-config.js
+++ build/build-config.js
@@ -1,7 +1,15 @@
 const fs = require('fs')
 const webpackConfig = require('./webpack.prod.conf')
 
-fs.writeFile('webpackconfig.json', JSON.stringify(webpackConfig, null, '\t'), err => {
-  if (err) throw err;
-  console.log('配置文件已生成');
+fs.writeFile('webpackconfig.json',
+  JSON.stringify(webpackConfig, (key, value) => {
+    if(value instanceof RegExp){
+      return value.toString();
+    } else {
+      return value;
+    }
+  }, '\t'),
+  err => {
+    if (err) throw err;
+    console.log('配置文件已生成');
 });

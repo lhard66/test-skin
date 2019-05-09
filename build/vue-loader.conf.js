030cb631604d625a7d8c4cab28d6af3e01eb410c
Allen Liu <lhard@qq.com>
2019-05-09 15:39:07 +0800

    vue文件设置全局scss变量

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,4 +1,5 @@
 'use strict'
+const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const isProduction = process.env.NODE_ENV === 'production'
@@ -6,14 +7,39 @@ const sourceMapEnabled = isProduction
   ? config.build.productionSourceMap
   : config.dev.cssSourceMap
 
+function getCssLoaders(sourceMapEnabled) {
+  // 增加scss全局变量
+  const cssLoaders = utils.cssLoaders({
+    sourceMap: sourceMapEnabled,
+    // extract: true,
+    extract: isProduction
+  });
+
+  if (cssLoaders.scss) {
+    cssLoaders.scss.push({
+      loader: 'sass-resources-loader',
+      options: {
+        sourceMap: sourceMapEnabled,
+        resources: [
+          path.join(__dirname, '../src/styles/common/_variables.scss')
+        ]
+      }
+    })
+  }
+
+  return cssLoaders;
+}
+
 module.exports = {
   // 当匹配到.vue文件时，文件内的样式的处理的loader
   // extract为false时，.vue文件中的样式将不被提取到一个独立的css文件中;true而反之。
+
+  // loaders: getCssLoaders(sourceMapEnabled),
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: true,
-    // extract: isProduction
+    extract: isProduction
   }),
+  // 给vue文件增加全局scss变量，避免频繁引入
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
   transformToRequire: {
